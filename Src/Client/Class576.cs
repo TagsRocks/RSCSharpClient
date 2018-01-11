718e551f82a1aa6a580247b83a3b6685d97e237b
FlareLine <xom.flareline@gmail.com>
2018-01-12 09:08:33 +1100

    Class576

---
+++ Src/Client/Class576.cs
@@ -0,0 +1,11 @@
+﻿namespace RSCSharpClient.Client
+{
+    public abstract class Class576
+    {
+        public Class576() { }
+
+        public abstract void Method6829(int i, int i_0_, int i_1_, int i_2_, float f, float f_3_, float f_4_, float f_5_, float[] fs, int i_6_);
+
+        public abstract void Method6830(int i, int i_7_, int i_8_, int i_9_, float f, float f_10_, float f_11_, float f_12_, float[] fs, int i_13_);
+    }
+}
